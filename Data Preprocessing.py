054578842a55063ad7d83e0d0eeae3f2298243a4
Tuan <tuanhoanh75@gmx.de>
2022-01-03 19:00:54 +0100

    Update: Data Prep Code

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -2,22 +2,20 @@ import pandas as pd
 import os
 
 # Get the list of all files and directories
-path = "C://Users/Tuan/Desktop/Output/Converted files"
-dir_list = os.listdir(path)
+PATH = "C://Users/Tuan/Desktop/Output/Converted_files/"
+dir_list = os.listdir(PATH)
 
-print("Files and directories in '", path, "' :")
-print(len(dir_list))
-
-"""
 # Read raw data; Reminder: Header starts at row 3 (internally in python, cuz index start with 0)
-df_kart = pd.read_csv('/Users/Tuan/Desktop/Output/Converted files/output_10361_1961-2020_Kart.csv',
-                      delimiter=';', header=3)
+for f in range(len(dir_list)):
+    file = PATH + dir_list[f]
+    df_file = pd.read_csv(file, delimiter=';', header=3)
 
-df = df_kart[df_kart.Jahr != 'Jahr']
-df.dropna(inplace=True)
-df = df.reset_index(drop=True)
+    df = df_file[df_file.Jahr != 'Jahr']
+    df.dropna(inplace=True)
+    df = df.reset_index(drop=True)
+
+    export_file = df.to_csv(PATH + 'export_' + dir_list[f], sep=';', index=False, header=True)
 
 # Export new formatted data file
-export_test = df.to_csv(r'/Users/Tuan/Desktop/Output/Converted files/export_10361_1961-2020_Kart.csv',
-                        sep=';', index=False, header=True)
-"""
+# export_test = df.to_csv(r'/Users/Tuan/Desktop/Output/Converted files/export_10361_1961-2020_Kart.csv',
+# sep=';', index=False, header=True)
