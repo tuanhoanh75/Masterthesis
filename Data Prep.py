a9f8861bc05ece6073f14ea688e2443a88d722a2
Tuan <tuanhoanh75@gmx.de>
2022-01-05 15:41:23 +0100

    Update v1.2: combined "Jahr" and "Tag" to date format

--- Data Prep.py
+++ Data Prep.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import os
+import pandas as pd
 import datetime as dt
 
 # Get Files from directory
@@ -11,8 +11,16 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 # Read csv Data Files; e.g. Kartoffel and interpret decimal with dot instead comma
-df_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0,  decimal=',')
+df_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0, decimal=',')
 
-# Replace commas with dots, which is required to convert object (string) to float
-print(df_kart.head(25))
-print(df_kart.info())
+# 1.) Convert columns "Jahr" and "Tag" to datetime format, whereby Day 1, 2019 can be translated to January 1st 2019
+# a) Create a new column
+df_kart["Combined"] = df_kart["Jahr"] * 1000 + df_kart["Tag"]
+
+# b) Convert current date format Year-Day to actual date
+df_kart["Date"] = pd.to_datetime(df_kart["Combined"], format="%Y%j")
+first_col = df_kart.pop("Date")             # shift column 'Name' to first position
+df_kart.insert(0, "Date", first_col)        # insert column using insert(position,column_name, first_column) function
+
+
+
