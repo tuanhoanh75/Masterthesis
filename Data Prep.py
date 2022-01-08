83823dd6fb6675c3a4bbb96eb51bc5bbbbf1e188
Tuan <tuanhoanh75@gmx.de>
2022-01-08 17:44:07 +0100

    Update: dtypes adjustment

--- Data Prep.py
+++ Data Prep.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import datetime as dt
@@ -16,12 +17,19 @@ file_list = os.listdir(PATH)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-# Read csv Data Files; e.g. Kartoffel and interpret decimal with dot instead comma
-df_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0, decimal=',')
+# Read csv Data Files; e.g. Kartoffel and interpret decimal with dot instead comma; Taking int32 instead default
+# int64 or float64 respectively for saving memory
+df_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0,
+                      decimal=',', usecols=["Jahr", "Tag", "BOF (%nFK)", "BOF (Vol%)"],
+                      dtype={"Jahr": np.int32, "Tag": np.int32, "BOF (%nFK)": np.float16, "BOF (Vol%)": np.float16})
+
 
 ## Convert columns "Jahr" and "Tag" to datetime format, whereby Day 1, 2019 can be translated to January 1st 2019
 # a) Create a new column
 df_kart["Combined"] = df_kart["Jahr"] * 1000 + df_kart["Tag"]
+
+print(df_kart.head())
+print(df_kart.info())
 
 # b) Convert current date format Year-Day to actual date
 df_kart["Date"] = pd.to_datetime(df_kart["Combined"], format="%Y%j")
@@ -30,7 +38,6 @@ df_kart.insert(0, "Date", first_col)        # insert column using insert(positio
 
 ## At first plot multiple raw time series data
 # Set the date column as the index of your DataFrame
-print(df_kart[350:370])
 df_kart = df_kart.set_index("Date")
 
 print("\n")
@@ -38,14 +45,10 @@ print("\n")
 # Print the summary statistics of the DataFrame
 df_kart = df_kart.drop(columns=["Jahr", "Tag", "Combined"])
 
-ax = df_kart["BOF (Vol%)"]["2003-01-01":"2003-12-31"].plot(linewidth=1, fontsize=10)
-
+ax = df_kart["BOF (%nFK)"]["2018-01-01":"2018-12-31"].plot(linewidth=1, fontsize=10)
 
 # Additional customizations
 ax.set_xlabel('Date')
 ax.legend(fontsize=10)
 
 plt.show()
-
-
-
