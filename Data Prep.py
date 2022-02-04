0730ea9770aaf4537cc13b1dd410dfab695e5ff7
tuanhoanh75 <39429626+tuanhoanh75@users.noreply.github.com>
2022-02-04 17:30:44 +0100

    new export csv files; and re-processing data

--- Data Prep.py
+++ Data Prep.py
@@ -1,17 +1,19 @@
 import os
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import datetime as dt
-from matplotlib import pyplot as plt
+#import seaborn as sns
+#import datetime as dt
+#from matplotlib import pyplot as plt
 
 # Presets
-plt.rcParams['figure.figsize'] = (10,5)
-plt.style.use('fivethirtyeight')
+#plt.rcParams['figure.figsize'] = (12,6)
+#plt.style.use('fivethirtyeight')
 
-# Get Files from directory
-PATH = r"/Users/Tuan/Masterarbeit/Data/Converted_files/csv"
+# Get Files from directory --> Please use your directory
+PATH = r"/home/aegis/Dokumente/Masterarbeit/Data/Converted_files/csv"
+PATH_2 = r"/home/aegis/Dokumente/Masterarbeit/Data/Converted_files/"
 file_list = os.listdir(PATH)
+
 
 # Presets: Show maximum columns and in one row
 pd.set_option('display.max_columns', None)
@@ -19,37 +21,93 @@ pd.set_option('display.width', None)
 
 # Read csv Data Files; e.g. Kartoffel and interpret decimal with dot instead comma; Taking int32 instead default
 # int64 or float64 respectively for saving memory
-df_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0,
+for f in range(len(file_list)):
+    df_daily = pd.read_csv(PATH + "/" + file_list[f], delimiter=';', 
+                       header=0, decimal=',', usecols=["Jahr", "Tag", "BOF (%nFK)", "BOF (Vol%)"], 
+                       dtype={"Jahr": np.int32, "Tag": np.int32, "BOF (%nFK)": np.float32, "BOF (Vol%)": np.float32})
+    
+    ## Convert cols "Jahr" and "Tag" to datetime format, whereby Day 1, 2019 can be translated to jan 1st 2019
+    # a.) Create a new col
+    df_daily["Combined"] = df_daily["Jahr"] * 1000 + df_daily["Tag"]
+    
+    # b.) Convert current date format Year-Day to actual date
+    df_daily["Date"] = pd.to_datetime(df_daily["Combined"], format="%Y%j")
+    daily = df_daily.pop("Date")              # shift column 'Name' to first position
+    df_daily.insert(0, "Date", daily)         # insert column using insert(position,column_name, first_column) function
+    
+    # c) Quick resample data from daily to weekly level
+    df_weekly = df_daily.drop(columns=["Jahr", "Tag", "Combined", "BOF (Vol%)"])
+    df_weekly = df_daily.resample("W", on="Date").mean()
+    
+    # Quick reset datetime index to column + re-arrange column order
+    df_weekly["Date"] = df_weekly.index
+    df_weekly = df_weekly.reset_index(drop=True)
+    weekly = df_weekly.pop("Date")                    # shift column 'Name' to first position
+    df_weekly.insert(0, "Date", weekly)
+    
+    # Omit superfluous columns like above
+    df_daily = df_daily.drop(columns=["Jahr", "Tag", "Combined", "BOF (Vol%)"])
+    
+    file_daily = 'daily' + file_list[f]
+    file_weekly = 'weekly' + file_list[f]
+    
+    export_daily = df_daily.to_csv(PATH_2 + 'Daily' + '/' + 'daily_' + file_list[f], 
+                                   sep=';', index=False, header=True, encoding='utf-8')
+    
+    export_weekly = df_weekly.to_csv(PATH_2 + 'Weekly' + '/' + 'weekly' + file_list[f], 
+                                   sep=';', index=False, header=True, encoding='utf-8')
+
+
+#########
+
+"""
+df_daily_kart = pd.read_csv(PATH + "/" + file_list[1], delimiter=';', header=0,
                       decimal=',', usecols=["Jahr", "Tag", "BOF (%nFK)", "BOF (Vol%)"],
-                      dtype={"Jahr": np.int32, "Tag": np.int32, "BOF (%nFK)": np.float16, "BOF (Vol%)": np.float16})
+                      dtype={"Jahr": np.int32, "Tag": np.int32, "BOF (%nFK)": np.float32, "BOF (Vol%)": np.float32})
 
 
 ## Convert columns "Jahr" and "Tag" to datetime format, whereby Day 1, 2019 can be translated to January 1st 2019
 # a) Create a new column
-df_kart["Combined"] = df_kart["Jahr"] * 1000 + df_kart["Tag"]
-
-print(df_kart.head())
-print(df_kart.info())
+df_daily_kart["Combined"] = df_daily_kart["Jahr"] * 1000 + df_daily_kart["Tag"]
 
 # b) Convert current date format Year-Day to actual date
-df_kart["Date"] = pd.to_datetime(df_kart["Combined"], format="%Y%j")
-first_col = df_kart.pop("Date")             # shift column 'Name' to first position
-df_kart.insert(0, "Date", first_col)        # insert column using insert(position,column_name, first_column) function
+df_daily_kart["Date"] = pd.to_datetime(df_daily_kart["Combined"], format="%Y%j")
+first_col = df_daily_kart.pop("Date")             # shift column 'Name' to first position
+df_daily_kart.insert(0, "Date", first_col)        # insert column using insert(position,column_name, first_column) function
 
 # c) Quick resample data from daily to weekly level
-df_weekly = df_kart.drop(columns=["Jahr", "Tag", "Combined", "BOF (Vol%)"])         # Omit superfluous columns
-df_weekly = df_weekly.resample('W', on='Date').mean()                               # With on='Date' it sets automatically column "Date"  as index
+df_daily_weekly = df_daily_kart.drop(columns=["Jahr", "Tag", "Combined", "BOF (Vol%)"])         # Omit superfluous columns
+df_daily_weekly = df_daily_weekly.resample('W', on='Date').mean()                               # With on='Date' it sets automatically column "Date"  as index
+
+
+# Quick reset datetime index to column + re-arrange column order 
+df_daily_weekly['Date'] = df_daily_weekly.index
+df_daily_weekly = df_daily_weekly.reset_index(drop=True)
+var_shift = df_daily_weekly.pop("Date")
+df_daily_weekly.insert(0, "Date", var_shift)
+
+
+# Omit superfluous columns like above
+df_daily_kart = df_daily_kart.drop(columns=["Jahr", "Tag", "Combined", "BOF (Vol%)"])
+
+
+export_daily = df_daily_kart.to_csv(PATH + '/' + 'daily_' + file_list[1], 
+                             sep=';', index=False, header=True, encoding='utf-8')
+
+export_weekly = df_daily_weekly.to_csv(PATH + '/' + 'weekly_' + file_list[1], 
+                               sep=';', index=False, header=True, encoding='utf-8')
 
 ## At first plot multiple raw time series data
 # Set the date column as the index of your DataFrame
-df_kart = df_kart.set_index("Date")
+#df_daily_kart = df_daily_kart.set_index("Date")
 
-print("\n")
-
-# Print the summary statistics of the DataFrame
-df_kart = df_kart.drop(columns=["Jahr", "Tag", "Combined"])
 y_ticks = np.arange(0, 110, 10)
-
 plt.yticks(y_ticks)
-ax_daily = df_kart["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, fontsize=10)
-ax_weekly = df_weekly["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, fontsize=10)
+ax_daily = df_daily_kart["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, fontsize=10)
+ax_weekly = df_daily_weekly["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, fontsize=10)
+
+## Try out some naiv clustering (e.g. hierarchical) approach
+"""
+
+
+
