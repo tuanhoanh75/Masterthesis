007b41c7a03646013274737f7099393d4f07f3d7
Tuan <aegis@pop-os.localdomain>
2022-02-24 16:09:50 +0100

    Clustering by example

--- Cluster.py
+++ Cluster.py
@@ -8,6 +8,7 @@ Created on Wed Feb 23 15:47:09 2022
 
 import os
 import re
+import math
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -26,11 +27,9 @@ file_container = sorted(os.listdir(str(DIR) + "/" + "hdf5"))
 plt.rcParams['figure.figsize'] = (26,14)
 plt.style.use('fivethirtyeight')
 
-
 # Get all files from file container
 data_file = pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_daily.hdf5")
 read_files = data_file.keys()                 # keys() list all records in the file container
-
 
 # Read data sets from file container - get daily first
 # Sample: Crop="Kartoffeln", Station=10361, Period=1961-2020
@@ -41,56 +40,51 @@ with pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_daily.hdf5") as store_dai
     
 # Quick plot data; set col "Date" as index
 data_KART = data_KART.set_index("Date")
-
-daily_1961 = data_KART["BOF (%nFK)"]["1961-01-01":"1961-12-31"]
-daily_2017 = data_KART["BOF (%nFK)"]["2017-01-01":"2017-12-31"]
-daily_2018 = data_KART["BOF (%nFK)"]["2018-01-01":"2018-12-31"]
-daily_2019 = data_KART["BOF (%nFK)"]["2019-01-01":"2019-12-31"]
+"""
+daily_1961 = data_KART["BOF (%nFK)"]["1961-01-01":"1962-12-31"]
+daily_2017 = data_KART["BOF (%nFK)"]["2015-01-01":"2016-12-31"]
+daily_2018 = data_KART["BOF (%nFK)"]["2017-01-01":"2018-12-31"]
+daily_2019 = data_KART["BOF (%nFK)"]["2019-01-01":"2020-12-31"]
 
 # Init the subplot function using number of rows and columns
 figure, axis = plt.subplots(2,2)
+figure.suptitle('Series sample of Kartoffel; Station: 10361, Period: 1961-2020')
+
 
 axis[0,0].plot(daily_1961, linewidth=1)
-axis[0,0].set_title("Year 1961")
+axis[0,0].set_title("Year 1961-1962")
 
 
-axis[0,1].plot(daily_1962, linewidth=1)
-axis[0,1].set_title("Year 2017")
+axis[0,1].plot(daily_2017, linewidth=1)
+axis[0,1].set_title("Year 2015-2016")
 
 axis[1,0].plot(daily_2018, linewidth=1)
-axis[1,0].set_title("Year 2018")
+axis[1,0].set_title("Year 2017-2018")
 
 axis[1,1].plot(daily_2019, linewidth=1)
-axis[1,1].set_title("Year 2019")
-
-
-
-
+axis[1,1].set_title("Year 2019-2020")
 """
-axis[0,0].data_KART["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, fontsize=8)
-axis[0,0].set_title("Year 1961")
 
-axis[0,1].data_KART["BOF (%nFK)"]["1962-01-01":"1962-12-31"].plot(linewidth=1, fontsize=8)
-axis[0,1].set_title("Year 1962")
+# # Normalize the data, which is actually not necessary, since all data series have the same scale
+# First instantiate MinMaxScaler
+scaler = MinMaxScaler()
+data_norm = scaler.fit_transform(data_KART)
+data_norm = data_norm.reshape(len(data_norm))
+data_norm = pd.DataFrame(data_norm, columns=['BOF (%nFk)'])
 
-axis[1,0].data_KART["BOF (%nFK)"]["2018-01-01":"2018-12-31"].plot(linewidth=1, fontsize=8)
-axis[1,0].set_title("Year 2018")
+# 1.) K-Means with euclidean distance metric
+# A good rule of thumb is choosing k as the square root of the number of points in the training data set
+cluster_n = math.ceil(math.sqrt(len(data_norm[0:365])))
 
-axis[1,1].data_KART["BOF (%nFK)"]["2019-01-01":"2019-12-31"].plot(linewidth=1, fontsize=8)
-axis[1,1].set_title("Year 2019")
+km = TimeSeriesKMeans(n_clusters=cluster_n, metric="euclidean")
+labels = km.fit_predict(data_norm)
 
-#y_ticks = np.arange(0, 110, 10)
-#plt.yticks(y_ticks)
-daily_1961 = data_KART["BOF (%nFK)"]["1961-01-01":"1961-12-31"].plot(linewidth=1, 
-                                                                   fontsize=14)
-daily_1962 = data_KART["BOF (%nFK)"]["1962-01-01":"1962-12-31"].plot(linewidth=1, 
-                                                                   fontsize=14)
-daily_2018 = data_KART["BOF (%nFK)"]["2018-01-01":"2018-12-31"].plot(linewidth=1, 
-                                                                   fontsize=14)
-daily_2019 = data_KART["BOF (%nFK)"]["2019-01-01":"2019-12-31"].plot(linewidth=1, 
-                                                                   fontsize=14)
+# Plot cluster result
+plot_n = cluster_n
 
-ax_daily.set_ylabel("BOF (%nFK)")
-"""
+
+
+
+
 
 
--- Playground.py
+++ Playground.py
@@ -1,11 +1,143 @@
-import pandas as pd
-import numpy as np
-from pathlib import Path
 import os
+import re
+import math
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from matplotlib import pyplot as plt
+# Preproccesing
+from sklearn.preprocessing import MinMaxScaler
+# Algorithms 
+from tslearn.barycenters import dtw_barycenter_averaging
+from tslearn.clustering import TimeSeriesKMeans
+from sklearn.cluster import KMeans
 
-data_folder = Path("/home/aegis/Dokumente/Masterarbeit/Data/Converted_files/csv")
 
-file_list = sorted(os.listdir(data_folder))
+directory = '/home/aegis/Dokumente/Masterarbeit/archive/'
 
-print(file_list)
+MySeries = []
+namesofMySeries = []
 
+# Plot data
+
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        df = pd.read_csv(directory + filename)
+        df = df.loc[:,["date", "value"]]
+        
+        df.set_index("date", inplace=True)
+        df.sort_index(inplace=True)
+        
+        MySeries.append(df)
+        namesofMySeries.append(filename[:-4])
+
+fig, axs = plt.subplots(6,4, figsize=(25,25))
+fig.suptitle('Series')
+
+for i in range(6):
+    for j in range(4):
+        if i*4+j+1 > len(MySeries):
+            continue
+        axs[i, j].plot(MySeries[i*4+j].values)
+        axs[i, j].set_title(namesofMySeries[i*4+j])
+        
+
+# Preprocessing        
+
+series_len = {len(series) for series in MySeries}
+
+ind = 0
+for series in MySeries:
+    print("[" + str(ind) + "]" + series.index[0] + " " + series.index[len(series) - 1])
+    ind+=1
+
+
+max_len = max(series_len)
+longest_series = None
+for series in MySeries:
+    if len(series) == max_len:
+        longest_series = series
+        
+
+problems_index = []
+
+for i, elem in enumerate(MySeries):
+    if len(MySeries[i]) != max_len:
+        problems_index.append(i)
+        MySeries[i] = MySeries[i].reindex(longest_series.index)
+
+
+def nan_counter(list_of_series):
+    nan_polluter_series_counter = 0
+    for series in list_of_series:
+        if series.isnull().sum().sum() > 0:
+            nan_polluter_series_counter+=1
+    print(nan_polluter_series_counter)
+    
+    
+for i in problems_index:
+    MySeries[i].interpolate(limit_direction="both", inplace=True)
+    
+    
+for i, elem in enumerate(MySeries):
+    scaler = MinMaxScaler()
+    MySeries[i] = scaler.fit_transform(MySeries[i])
+    MySeries[i] = MySeries[i].reshape(len(MySeries[i]))
+
+
+# Clustering using Euclidean and DTW
+cluster_n = math.ceil(math.sqrt(len(MySeries)))
+
+km_euclidean = TimeSeriesKMeans(n_clusters=cluster_n, metric="euclidean")
+km_dtw = TimeSeriesKMeans(n_clusters=cluster_n, metric="dtw")
+
+labels_eu = km_euclidean.fit_predict(MySeries)
+labels_dtw = km_dtw.fit_predict(MySeries)
+
+
+# Plotting cluster result
+
+plot_n = cluster_n
+
+fig, axs = plt.subplots(plot_n, plot_n, figsize=(25,25))
+fig.suptitle("Clusters")
+
+row_i = 0
+column_j = 0
+
+# For each label there is plots every series with label
+for label in set(label):
+    None
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+        
