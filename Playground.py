9eb02b146aee7099509a76495e9c84ad7f25137c
Tuan <aegis@pop-os.localdomain>
2022-02-25 17:00:14 +0100

    Cluster insight by example

--- Playground.py
+++ Playground.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tslearn.barycenters import dtw_barycenter_averaging
 from tslearn.clustering import TimeSeriesKMeans
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 
 directory = '/home/aegis/Dokumente/Masterarbeit/archive/'
@@ -97,39 +98,163 @@ labels_dtw = km_dtw.fit_predict(MySeries)
 
 # Plotting cluster result
 
-plot_n = cluster_n
+plot_n = math.ceil(math.sqrt(cluster_n))
 
 fig, axs = plt.subplots(plot_n, plot_n, figsize=(25,25))
-fig.suptitle("Clusters")
+fig.suptitle("Clusters with euclidean and np.average")
 
 row_i = 0
 column_j = 0
 
 # For each label there is plots every series with label
-for label in set(label):
-    None
+# With euclidean distance metric 
+for label in set(labels_eu):
+    cluster_eu = []
+    for i, elem in enumerate(labels_eu):
+        if (labels_eu[i] == label):
+            axs[row_i, column_j].plot(MySeries[i], c="gray", alpha=0.4)
+            cluster_eu.append(MySeries[i])
+        
+    if len(cluster_eu) > 0:
+        axs[row_i, column_j].plot(np.average(np.vstack(cluster_eu), axis=0), c="red")
+        axs[row_i, column_j].set_title("Cluster " + str(row_i*cluster_n+column_j))
+        column_j += 1
+        
+    if column_j%plot_n == 0:
+        row_i += 1
+        column_j = 0
+
+        
+
+####################################################################################################
+
+plot_n = math.ceil(math.sqrt(cluster_n))
+
+fig, axs = plt.subplots(plot_n, plot_n, figsize=(25,25))
+fig.suptitle("Clusters with dtw and np.averaging")
+
+row_i = 0
+column_j = 0
+
+
+# With dtw distance metric 
+for label in set(labels_dtw):
+    cluster_dtw = []
+    for i, elem in enumerate(labels_dtw):
+        if (labels_dtw[i] == label):
+            axs[row_i, column_j].plot(MySeries[i], c="gray", alpha=0.4)
+            cluster_dtw.append(MySeries[i])
+        
+    if len(cluster_dtw) > 0:
+        axs[row_i, column_j].plot(np.average(np.vstack(cluster_dtw), axis=0), c="red")
+        axs[row_i, column_j].set_title("Cluster " + str(row_i*cluster_n+column_j))
+        column_j += 1
+        
+    if column_j%plot_n == 0:
+        row_i += 1
+        column_j = 0
+
+
+
+plot_n = math.ceil(math.sqrt(cluster_n))
+
+fig, axs = plt.subplots(plot_n, plot_n, figsize=(25,25))
+fig.suptitle("Clusters with dtw and dtw_barycenter_averaging")
+
+row_i = 0
+column_j = 0
+
+
+# With dtw distance metric 
+for label in set(labels_dtw):
+    cluster_dtw = []
+    for i, elem in enumerate(labels_dtw):
+        if (labels_dtw[i] == label):
+            axs[row_i, column_j].plot(MySeries[i], c="gray", alpha=0.4)
+            cluster_dtw.append(MySeries[i])
+        
+    if len(cluster_dtw) > 0:
+        axs[row_i, column_j].plot(dtw_barycenter_averaging(np.vstack(cluster_dtw)), c="red")
+        axs[row_i, column_j].set_title("Cluster " + str(row_i*cluster_n+column_j))
+        column_j += 1
+        
+    if column_j%plot_n == 0:
+        row_i += 1
+        column_j = 0
+        
+        
+# Cluster Distribution for eu and dtw
+cluster_c_eu = [len(labels_eu[labels_eu==i]) for i in range(cluster_n)]
+cluster_num_eu = ["Cluster " + str(i) for i in range(cluster_n)]
+
+plt.figure(figsize=(15,5))
+plt.title("Cluster Distribution for KMeans with eu")
+plt.bar(cluster_num_eu, cluster_c_eu)
+
+cluster_c_dtw = [len(labels_dtw[labels_dtw==i]) for i in range(cluster_n)]
+cluster_num_dtw = ["Cluster " + str(i) for i in range(cluster_n)]
+
+plt.figure(figsize=(15,5))
+plt.title("Cluster Distribution for KMeans with dtw")
+plt.bar(cluster_num_dtw, cluster_c_dtw)
+
+
+# Cluster Mapping
+names_for_labels_eu = [f"Cluster {label}" for label in labels_eu]
+pd.DataFrame(zip(namesofMySeries, names_for_labels_eu), 
+             columns=["Series", "Cluster"]).sort_values(by="Cluster").set_index("Series")
+
+names_for_labels_dtw = [f"Cluster {label}" for label in labels_dtw]
+pd.DataFrame(zip(namesofMySeries, names_for_labels_dtw), 
+             columns=["Series", "Cluster"]).sort_values(by="Cluster").set_index("Series")
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+# Curse of Dimensionality
+pca = PCA(n_components=2)
+MySeries_transformed = pca.fit_transform(MySeries)
+
+plt.figure(figsize=(25,10))
+plt.scatter(MySeries_transformed[:,0], MySeries_transformed[:, 1], s=300)
+
+
+kmeans = KMeans(n_clusters=7, max_iter=5000)
+labels_pca_transformed = kmeans.fit_predict(MySeries_transformed)
+
+plt.figure(figsize=(25,10))
+plt.scatter(MySeries_transformed[:,0], MySeries_transformed[:, 1], 
+            c=labels_pca_transformed, s=300)
+
+
+
+###################################################################################
+
+
+
+plot_n = math.ceil(math.sqrt(cluster_n))
+
+fig, axs = plt.subplots(plot_n, plot_n, figsize=(25,25))
+fig.suptitle("Clusters with PCA transformed series")
+
+row_i = 0
+column_j = 0
+
+
+# With dtw distance metric 
+for label in set(labels_pca_transformed):
+    cluster_pca = []
+    for i, elem in enumerate(labels_pca_transformed):
+        if (labels_pca_transformed[i] == label):
+            axs[row_i, column_j].plot(MySeries[i], c="gray", alpha=0.4)
+            cluster_pca.append(MySeries[i])
+        
+    if len(cluster_pca) > 0:
+        axs[row_i, column_j].plot(np.average(np.vstack(cluster_pca), axis=0), c="red")
+        axs[row_i, column_j].set_title("Cluster " + str(row_i*8+column_j))
+        column_j += 1
+        
+    if column_j%plot_n == 0:
+        row_i += 1
+        column_j = 0
     
     
     
