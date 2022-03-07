8200e6ab1cfc46db07fe6baa439b9c4770283f2f
Tuan <tuanhoanh75@gmx.de>
2022-03-07 18:45:08 +0100

    Clustering nFK and plot results

--- Cluster.py
+++ Cluster.py
@@ -7,11 +7,9 @@ Created on Wed Feb 23 15:47:09 2022
 """
 
 import os
-import re
 import math
 import numpy as np
 import pandas as pd
-import dateutil
 from pathlib import Path
 import datetime as dt
 from matplotlib import pyplot as plt
@@ -30,74 +28,169 @@ file_container = sorted(os.listdir(str(DIR) + "/" + "hdf5"))
 plt.rcParams['figure.figsize'] = (26,14)
 plt.style.use('fivethirtyeight')
 
-# Get all files from file container
-data_file = pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_daily.hdf5")
-read_files = data_file.keys()                 # keys() list all records in the file container
+# Get all files from file container for daily data set
+daily_file = pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_daily.hdf5")
+daily_files = daily_file.keys()                 # keys() list all records in the file container
+
+# Get all files from file container for weekly data set
+weekly_file = pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_weekly.hdf5")
+weekly_files = weekly_file.keys()  
+
 
 # Read data sets from file container - get daily first
 # Sample: Crop="Kartoffeln", Station=10361, Period=1961-2020
 with pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_daily.hdf5") as store_daily:
     
-    mySeries = []
+    tmpSeries = []
+    mySeries_daily = []
     
-    data_KART = store_daily[read_files[0]]
-    meta_KART = store_daily.get_storer(read_files[0]).attrs.meta_data
+    data_KART = store_daily[daily_files[0]]
+    meta_KART_daily = store_daily.get_storer(daily_files[0]).attrs.meta_data
     data_KART['Date'] = data_KART['Date'].dt.strftime('%Y-%m-%d')
 
     data_KART.set_index('Date', inplace=True)
     data_KART.sort_index(inplace=True)
     
     # Partition dataframe data_KART by years
-    agg = data_KART.groupby(pd.DatetimeIndex(data_KART.index).year)
-    for y in agg:
-        mySeries.append(y)
+    #agg = data_KART.groupby(pd.DatetimeIndex(Samuel Kimdata_KART.index).year)
+    for y in data_KART.groupby(pd.DatetimeIndex(data_KART.index).year):
+        tmpSeries.append(y)
+    
+    # Get rid of the tuple     
+    for i, elem in enumerate(tmpSeries):
+        mySeries_daily.append(elem[1])
+    
+    # Delete unnecessary (tmp) variables, which are not needed in further analysis
+    del tmpSeries, elem, i, y, data_KART
 
- 
 """
-daily_1961 = data_KART["BOF (%nFK)"]["1961-01-01":"1962-12-31"]
-daily_2017 = data_KART["BOF (%nFK)"]["2015-01-01":"2016-12-31"]
-daily_2018 = data_KART["BOF (%nFK)"]["2017-01-01":"2018-12-31"]
-daily_2019 = data_KART["BOF (%nFK)"]["2019-01-01":"2020-12-31"]
+# Read data sets from file container - get weekly first
+# Sample: Crop="Kartoffeln", Station=10361, Period=1961-2020
+with pd.HDFStore(str(DIR) + "/" + "hdf5" + "/" + "data_weekly.hdf5") as store_weekly:
+    
+    tmpSeries = []
+    mySeries_weekly = []
+    
+    data_KART = store_weekly[weekly_files[0]]
+    meta_KART_weekly = store_weekly.get_storer(weekly_files[0]).attrs.meta_data
+    data_KART['Date'] = data_KART['Date'].dt.strftime('%Y-%m-%d')
 
-# Init the subplot function using number of rows and columns
-figure, axis = plt.subplots(2,2)
-figure.suptitle('Series sample of Kartoffel; Station: 10361, Period: 1961-2020')
-
-
-axis[0,0].plot(daily_1961, linewidth=1)
-axis[0,0].set_title("Year 1961-1962")
-
-
-axis[0,1].plot(daily_2017, linewidth=1)
-axis[0,1].set_title("Year 2015-2016")
-
-axis[1,0].plot(daily_2018, linewidth=1)
-axis[1,0].set_title("Year 2017-2018")
-
-axis[1,1].plot(daily_2019, linewidth=1)
-axis[1,1].set_title("Year 2019-2020")
+    data_KART.set_index('Date', inplace=True)
+    data_KART.sort_index(inplace=True)
+    
+    
+    # Partition dataframe data_KART by years
+    #agg = data_KART.groupby(pd.DatetimeIndex(data_KART.index).year)
+    for y in data_KART.groupby(pd.DatetimeIndex(data_KART.index).year):
+        tmpSeries.append(y)
+    
+    # Get rid of the tuple     
+    for i, elem in enumerate(tmpSeries):
+        mySeries_weekly.append(elem[1])
+    
+    # Delete unnecessary (tmp) variables, which are not needed in further analysis
+    del tmpSeries, elem, i, y, data_KART
 """
+
+# Create label 'year'
+tmp = []
+year = []
+
+for i, elem in enumerate(mySeries_daily):
+    y = pd.DatetimeIndex(elem.index).year
+    dupl = list(dict.fromkeys(y))
+    tmp.append(dupl)
+
+for j, elem in enumerate(tmp):
+    year.extend(elem)
+
+year = pd.DataFrame(year, columns=['Jahr'])
+# Delete unnecessary (tmp) variables, which are not needed in further analysis
+del tmp, dupl, elem, i, j, y
+    
 
 # # Normalize the data, which is actually not necessary, since all data series have the same scale
 # First instantiate MinMaxScaler
 scaler = MinMaxScaler()
-data_norm = scaler.fit_transform(data_KART)
-data_norm = data_norm.reshape(len(data_norm))
-data_norm = pd.DataFrame(data_norm, columns=['BOF (%nFk)'])
+mySeries_daily_norm = []
+for i, elem in enumerate(mySeries_daily):
+    tmp = scaler.fit_transform(mySeries_daily[i])
+    mySeries_daily_norm.append(tmp)
+    
+for j, elem in enumerate(mySeries_daily_norm):
+    mySeries_daily_norm[j] = mySeries_daily_norm[j].reshape(len(mySeries_daily_norm[j]))
 
-# 1.) K-Means with euclidean distance metric
+# Delete unnecessary (tmp) variables, which are not needed in further analysis
+del tmp, elem, i, j
+
+
+# 1.) K-Means
 # A good rule of thumb is choosing k as the square root of the number of points in the training data set
-cluster_n = math.ceil(math.sqrt(len(data_norm[0:365])))
+cluster_n = math.ceil(math.sqrt(len(mySeries_daily_norm)))
 
-km = TimeSeriesKMeans(n_clusters=cluster_n, metric="euclidean")
-labels = km.fit_predict(data_norm)
+# Euclidean distance metric
+km_euc = TimeSeriesKMeans(n_clusters=10, metric="euclidean", max_iter=1000)
+# DTW distance metric
+km_dtw = TimeSeriesKMeans(n_clusters=10, metric="dtw", max_iter_barycenter=1000)
 
-# Plot cluster result
-plot_n = cluster_n
-
-
-
-
+labels_euc = km_euc.fit_predict(mySeries_daily_norm)
+labels_dtw = km_dtw.fit_predict(mySeries_daily_norm)
 
 
+# 2.) Plot Results - for each label plot every series with that label
+plot_n = math.ceil(cluster_n / 2)
 
+fig, axs = plt.subplots(nrows=plot_n, ncols=plot_n,figsize=(25,25))
+fig.suptitle("Clusters of BOF (%nFK) - calucated with euclidean metric")
+
+row_i = 0
+col_j = 0
+
+# For euclidean distance
+for label in set(labels_euc):
+    cluster_eu = []
+    for i, elem in enumerate(labels_euc):
+        if (labels_euc[i] == label):
+            axs[row_i, col_j].plot(mySeries_daily_norm[i], c="gray", alpha=0.4)
+            cluster_eu.append(mySeries_daily_norm[i])
+            
+    if len(cluster_eu) > 0:
+        axs[row_i, col_j].plot(np.average(np.vstack(cluster_eu), axis=0), c="red")
+    
+    axs[row_i, col_j].set_title("Cluster " + str(row_i*cluster_n+col_j))
+    col_j +=1
+    
+    if col_j%plot_n == 0:
+        row_i +=1
+        col_j = 0
+
+
+fig, axs = plt.subplots(nrows=plot_n, ncols=plot_n,figsize=(25,25))
+fig.suptitle("Clusters of BOF (%nFK) - calucated with dtw metric")
+
+row_i = 0
+col_j = 0
+
+# For dtw distance metric
+for label in set(labels_dtw):
+    cluster_dtw = []
+    for i, elem in enumerate(labels_dtw):
+        if (labels_dtw[i] == label):
+            axs[row_i, col_j].plot(mySeries_daily_norm[i], c="gray", alpha=0.4)
+            cluster_dtw.append(mySeries_daily_norm[i])
+            
+    if len(cluster_dtw) > 0:
+        axs[row_i, col_j].plot(dtw_barycenter_averaging(np.vstack(cluster_dtw)), c="red")
+    
+    axs[row_i, col_j].set_title("Cluster " + str(row_i*cluster_n+col_j))
+    col_j +=1
+    
+    if col_j%plot_n == 0:
+        row_i +=1
+        col_j = 0
+
+
+# Quick plot
+mySeries_daily[14].plot(linewidth=1, fontsize=10)
+mySeries_daily[57].plot(linewidth=1, fontsize=10)
+mySeries_daily[58].plot(linewidth=1, fontsize=10)
