e3c84f56a2ea9b006dae441d7e15d8f849fe1b46
tuanhoanh75 <39429626+tuanhoanh75@users.noreply.github.com>
2022-04-21 15:34:29 +0200

    Rapid restructuring code
    Restructuring of the code and implementation of a validation logic, if the desired work and process directory exists, which is required for the subsequent analysis

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -1,6 +1,7 @@
 import os 
 #import re
 import pandas as pd
+from pathlib import Path
 
 """
 # Define regex pattern for substring extraction
@@ -12,29 +13,52 @@ pattern_vMainCrop = re.compile(r'[A-Z]+[a-zA-Z]{,3}')
 # Get current directoy
 cwd = os.getcwd()
 
-# Check if working directory already exist to process raw data if not create one
-if not os.path.exists(cwd + "\working_dir"):
+# Check exisiting working directory, if not create one
+if not os.path.exists(cwd + "/" + "working_dir"):
     os.makedirs("working_dir")
-    print("Working directoy successfully created")
+    print("Working directory successfully created")
+    
+    if not os.path.exists(cwd + "/" + "working_dir" + "/" + "processed_data"):
+        os.makedirs("processed_data")
+        print("Processed data directory successfully created")
+    else:
+        print("Processed data directory exist already")
 else:
-    print("Working directory exist already")
-
-"""
-DIR_PATH = Path("Users/Tuan/Masterarbeit/Data/raw-data/")
-CONV_PATH = r"C:/Users/Tuan/Masterarbeit/Data/conv_data/"
-
-list_files = sorted(os.listdir(DIR_PATH))
-
-for i, elem in enumerate(list_files):
-    raw_files = pd.read_csv(DIR_PATH + list_files[i], sep="\s+", header=3, usecols=[0,1,6], encoding='unicode_escape')
-
-    df_file = raw_files[(raw_files["Jahr"] != "Jahr") & 
-                        (raw_files["Jahr"] != "Station:") &
-                        (raw_files["Jahr"] != "Flexibilisierung:") & 
-                        (raw_files["Jahr"] != "Hauptfrucht:") & 
-                        (raw_files["Jahr"] != "mm")]
+    print("Working directory exist already, but check exisiting processed directory!")
     
+    if not os.path.exists(cwd + "/" + "working_dir" + "/" + "processed_data"):
+        os.makedirs(cwd + "/" + "working_dir" + "/" + "processed_data")
+        print("Processed data directory successfully created")
+    else:
+        print("Processed data directory exist already")
+
+
+# Define path variable
+# First path variable contains raw data
+WORK_PATH = Path(cwd + "/" + "working_dir")
+PROC_PATH = Path(cwd + "/" + "working_dir" + "/" + "processed_data")
+
+# List all files within working directory
+file_list = []
+filename_only = sorted([])
+for x in sorted(os.listdir(WORK_PATH)):
+    if x.endswith(".txt"):
+        file_list.append(x)
+
+        
+# Process raw data files and convert them to a manageable data format, like csv
+for i, elem in enumerate(file_list):
+    raw_files = pd.read_csv(str(WORK_PATH) + "/" + file_list[i], sep="\s+", header=3, usecols=[0,1,6], encoding='unicode_escape')
     
+    # Fltering data and remove rows that contains strings
+    df_file = raw_files.loc[raw_files["Jahr"] != "Station:", "Jahr":"BOF"]
+    df_file = raw_files.loc[raw_files["Jahr"] != "Flexibilisierung:", "Jahr":"BOF"]
+    df_file = raw_files.loc[raw_files["Jahr"] != "Hauptfrucht:", "Jahr":"BOF"]
+    
+    # Special treatment for Jahr and mm
+    df_file = raw_files.loc[raw_files["Jahr"].str.contains("Jahr|mm") == False, "Jahr":"BOF"]
+    
+    # Remove rows that contains na values
     df_file.dropna(inplace=True)
     df_file.reset_index(drop=True, inplace=True)
     
@@ -42,8 +66,7 @@ for i, elem in enumerate(list_files):
     df_file = df_file.rename(columns={df_file.columns[2]: "BOF (%nFK)"})
     
     # Check the number of days of a year, if the year contains more than 365 day then remove the day 366
-    
-    indexNames = df_file[df_file["Tag"] == "366"].index
+    indexNames = df_file.loc[df_file["Tag"] == "366", "Jahr":"BOF (%nFK)"].index
     df_file = df_file.drop(indexNames)
     
     # Quick dtype conversion, because per default all read data are of type object (or string)
@@ -68,7 +91,10 @@ for i, elem in enumerate(list_files):
     # Omit superfluous columns
     df_file = df_file.drop(columns=["Jahr", "Tag"])
     
+    # Filenames without extension .txt
+    base = os.path.basename(file_list[i])
+    tmp = os.path.splitext(base)
+    filename_only.append(tmp[0])
+    
     # export each converted data files, but without meta data
-    export_csv = df_file.to_csv(CONV_PATH + list_files[i] + ".csv", sep=";", index=False, header=True, encoding="utf-8")
-
-"""
+    export_csv = df_file.to_csv(str(PROC_PATH) + "/" + filename_only[i] + ".csv", sep=";", index=False, header=True, encoding="utf-8")
